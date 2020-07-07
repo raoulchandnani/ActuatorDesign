1706fff3dc5100cc1c44547d377cd23c8bb03708
Raoul Chandnani <raoulchandnani@tamu.edu>
2020-07-06 22:37:11 -0500

    3D Elastomer Model updated

--- Abaqus Scripts/3D_Elastomer.py
+++ Abaqus Scripts/3D_Elastomer.py
@@ -36,12 +36,14 @@ for i in range(len(Data)):
             Datalow[count]=Data[i]
             count=count+1
 Datalow=Datalow[~np.all(Datalow == 0, axis=1)]
-
+with open("Aero_Data_I.txt", "r") as a:
+    Datalow= [[float(num) for num in line.split(' ')] for line in a]
+Datalow=np.array(Datalow)
 s = mdb.models['Model-1'].ConstrainedSketch(name='__profile__', 
     sheetSize=200.0)
 g, v, d, c = s.geometry, s.vertices, s.dimensions, s.constraints
 s.setPrimaryObject(option=STANDALONE)
-p = mdb.models['Model-1'].Part(name='Part-1', dimensionality=THREE_D, 
+p = mdb.models['Model-1'].Part(name='Elastomer', dimensionality=THREE_D, 
 	type=DEFORMABLE_BODY)
 d2 = p.datums
 loftsectionsA=[]
@@ -49,7 +51,7 @@ for xloc in range(100):
 	if xloc>0:
 		DP=p.DatumPlaneByPrincipalPlane(principalPlane=XYPLANE, offset=0.0315*xloc)
 		DPP=DP.id
-		p = mdb.models['Model-1'].parts['Part-1']
+		p = mdb.models['Model-1'].parts['Elastomer']
 		DP=p.DatumAxisByTwoPoint(point1=(Datalow[-1,1],Datalow[0,2]-0.0315*xloc,0.0315*xloc), point2=(Datalow[-1,1],Datalow[0,2]+0.0315*xloc,0.0315*xloc))
 		DPE=DP.id
 		t = p.MakeSketchTransform(sketchPlane=d2[DPP], sketchUpEdge=d2[DPE], 
@@ -62,31 +64,67 @@ for xloc in range(100):
 		p.projectReferencesOntoSketch(sketch=s, filter=COPLANAR_EDGES)
 	XLOC=11.85+0.0315*xloc
 	count=0
-	Dataxloc=[[0.000000 for x in range(7)] for y in range(len(Datalow))]
+	Dataxloc=[[0.000000 for x in range(3)] for y in range(len(Datalow))]
 	Dataxloc=np.array(Dataxloc)
 	for i in range(len(Datalow)):
 		if abs(Datalow[i,0]-XLOC)<1E-06:
 			Dataxloc[count]=Datalow[i]
 			count=count+1
-	Dataxloc=Dataxloc[~np.all(Dataxloc == 0, axis=1)]	
-	for i in range(len(Dataxloc)-2):
-		s.Spline(points=((Dataxloc[i,1], Dataxloc[i,2]-Datalow[0,2]), (Dataxloc[i+1,1], Dataxloc[i+1,2]-Datalow[0,2])))
-	s.Line(point1=(Dataxloc[0,1], Dataxloc[0,2]-Datalow[0,2]), point2=(Dataxloc[0,1], Dataxloc[0,2]-Datalow[0,2]+0.01))
-	s.Line(point1=(Dataxloc[0,1], Dataxloc[0,2]-Datalow[0,2]+0.01), point2=(Dataxloc[-2,1], Dataxloc[-2,2]-Datalow[0,2]+0.01))
-	s.Line(point1=(Dataxloc[-2,1], Dataxloc[-2,2]-Datalow[0,2]), point2=(Dataxloc[-2,1], Dataxloc[-2,2]-Datalow[0,2]+0.01))	
-	p = mdb.models['Model-1'].parts['Part-1']
+	Dataxloc=Dataxloc[~np.all(Dataxloc == 0, axis=1)]
+	Pad_thk=-(min(Datalow[:,2])-min(Dataxloc[:,2]))-0.04#-(3.15-0.0315*xloc)*tan(0.06841917290255273)+0.04
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Left_Plate_2'].datums[DP8].pointOn
+	Pad_thk=DVct2[2]+Datalow[0,2]-0.18+(3.15-0.0315*xloc)*tan(0.06841917290255273)
+	for i in range(len(Dataxloc)-1):
+#		s.Spline(points=((Dataxloc[i,1], -(Dataxloc[i,2]-min(Datalow[:,2])-DVct2[2])), (Dataxloc[i+1,1], -(Dataxloc[i+1,2]-min(Datalow[:,2])-DVct2[2]))))
+		s.Spline(points=((Dataxloc[i,1], -(Dataxloc[i,2])), (Dataxloc[i+1,1], -(Dataxloc[i+1,2]))))
+	DVct=mdb.models['Model-1'].rootAssembly.instances['Left_Plate_2'].datums[DP12].pointOn
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Left_Plate_2'].datums[DP8].pointOn
+	s.Line(point1=(DVct[0]-Pad, DVct2[2]-Pad_thk), point2=(Dataxloc[0,1], -(Dataxloc[0,2])))
+#	s.Line(point1=(DVct[0]-Pad, DVct2[2]-Pad_thk), point2=(DVct[0]-Pad, -(Dataxloc[0,2])))
+	s.Line(point1=(DVct[0]-Pad, DVct2[2]-Pad_thk), point2=(DVct2[0], DVct2[2]-Pad_thk))
+	DVct=mdb.models['Model-1'].rootAssembly.instances['Left_Plate_2'].datums[DP5].pointOn
+	s.Line(point1=(DVct2[0], DVct2[2]-Pad_thk), point2=(DVct[0], DVct[2]-Pad_thk))
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Left_Plate'].datums[DP8].pointOn
+	s.Line(point1=(DVct[0], DVct[2]-Pad_thk), point2=(DVct2[0], DVct2[2]-Pad_thk))
+	DVct=mdb.models['Model-1'].rootAssembly.instances['Left_Plate'].datums[DP5].pointOn
+	s.Line(point1=(DVct2[0], DVct2[2]-Pad_thk), point2=(DVct[0], DVct[2]-Pad_thk))
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Middle_Plate'].datums[DP8].pointOn
+	s.Line(point1=(DVct[0], DVct[2]-Pad_thk), point2=(DVct2[0], DVct2[2]-Pad_thk))
+	DVct=mdb.models['Model-1'].rootAssembly.instances['Middle_Plate'].datums[DP5].pointOn
+	s.Line(point1=(DVct2[0], DVct2[2]-Pad_thk), point2=(DVct[0], DVct[2]-Pad_thk))
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Right_Plate'].datums[DP8].pointOn
+	s.Line(point1=(DVct[0], DVct[2]-Pad_thk), point2=(DVct2[0], DVct2[2]-Pad_thk))
+	DVct=mdb.models['Model-1'].rootAssembly.instances['Right_Plate'].datums[DP5].pointOn
+	s.Line(point1=(DVct2[0], DVct2[2]-Pad_thk), point2=(DVct[0], DVct[2]-Pad_thk))
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Right_Plate_2'].datums[DP8].pointOn
+	s.Line(point1=(DVct[0], DVct[2]-Pad_thk), point2=(DVct2[0], DVct2[2]-Pad_thk))
+	DVct=mdb.models['Model-1'].rootAssembly.instances['Right_Plate_2'].datums[DP5].pointOn
+	s.Line(point1=(DVct2[0], DVct2[2]-Pad_thk), point2=(DVct[0], DVct[2]-Pad_thk))
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Right_Plate_2'].datums[DP9].pointOn
+	DVct2=mdb.models['Model-1'].rootAssembly.instances['Left_Plate_2'].datums[DP8].pointOn
+	DVct3=mdb.models['Model-1'].rootAssembly.instances['Left_Plate_2'].datums[DP12].pointOn
+	s.Line(point1=(DVct[0], DVct[2]-Pad_thk), point2=(-DVct3[0]+Pad, DVct2[2]-Pad_thk))
+#	s.Line(point1=(Dataxloc[-1,1], -(Dataxloc[-1,2])), point2=(-DVct3[0]+Pad, -(Dataxloc[-2,2])))
+	s.Line(point1=(Dataxloc[-1,1], -(Dataxloc[-1,2])), point2=(-DVct3[0]+Pad, DVct2[2]-Pad_thk))
+
+	p = mdb.models['Model-1'].parts['Elastomer']
 	if xloc==0:
 		p.BaseWire(sketch=s)
 	else:
 		p.Wire(sketchPlane=d2[DPP], sketchUpEdge=d2[DPE], sketchPlaneSide=SIDE1, 
 			sketchOrientation=RIGHT, sketch=s)
 	s.unsetPrimaryObject()
-	p = mdb.models['Model-1'].parts['Part-1']
+	p = mdb.models['Model-1'].parts['Elastomer']
 	del mdb.models['Model-1'].sketches['__profile__']
-	p = mdb.models['Model-1'].parts['Part-1']
-	p = mdb.models['Model-1'].parts['Part-1']
+	p = mdb.models['Model-1'].parts['Elastomer']
+	p = mdb.models['Model-1'].parts['Elastomer']
 	e = p.edges
 	edges = e.getByBoundingBox(-1000000,-1000000,0.0315*xloc-0.0001,1000000,1000000,0.0315*xloc+0.0001)
 	p.Set(edges=edges, name='E%i'%xloc)
-p.SolidLoft(loftsections=[p.sets['E%i'%xloc].edges[:] for xloc in range(100)], startCondition=NORMAL, endCondition=NORMAL)
-
+xlist=[]
+for xloc in range(100):
+	xlist.append(xloc)
+p.SolidLoft(loftsections=[p.sets['E%i'%xloc].edges[:] for xloc in xlist], startCondition=NORMAL, endCondition=NORMAL)
+# p.SolidLoft(loftsections=[p.sets['E%i'%xloc].edges[:] for xloc in range(43,71)], startCondition=SPECIFIED, 
+    # startTangent=90.0, startMagnitude=5.0, endCondition=SPECIFIED, 
+    # endTangent=90.0, endMagnitude=5.0)
